4cb515a1f64f8d47294af4497003d8bab85f2149
Matthew Kosarek <matt.kosarek@canonical.com>
2024-08-11 16:16:21 -0400

    Initial commit with a build script for ease of use

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # miracle-de-on-nwg-shell
-Dotfiels for a miracle desktop environment based on nwg-shell
+This project defines a full shell based on [miracle-wm](https://github.com/mattkae/miracle-wm)
+and powered by [nwg-shell](https://github.com/nwg-piotr/nwg-shell).
+
+## Components
+- [swaybg](https://github.com/swaywm/swaybg)
+- [nwg-panel](https://github.com/nwg-piotr/nwg-panel)
+- [nwg-dock](https://github.com/nwg-piotr/nwg-dock)
+- [nwg-drawer](https://github.com/nwg-piotr/nwg-drawer)
+- [nwg-bar](https://github.com/nwg-piotr/nwg-bar)
+
+
+## Building
+To build all of the subprojects from source, you may run the `build.sh` script at the root of
+this project. **Please note that this does NOT install all of the dependencies of the projects.**
+
---
+++ build.sh
@@ -0,0 +1,48 @@
+echo "Building latest miracle-wm..."
+git clone https://github.com/mattkae/miracle-wm.git
+cd miracle-wm
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+cd ../..
+
+echo "Building swaybg"
+git clone https://github.com/swaywm/swaybg.git
+cd swaybg
+meson build/
+ninja -C build/
+sudo ninja -C build/ install
+cd ..
+
+echo "Building nwg-panel..."
+git clone https://github.com/nwg-piotr/nwg-panel.git
+git clone https://github.com/nwg-piotr/nwg-icon-picker
+cd nwg-panel
+sudo ./install.sh && cd ../nwg-icon-picker && sudo ./install.sh
+cd ..
+
+echo "Building nwg-dock..."
+git clone https://github.com/nwg-piotr/nwg-dock.git
+cd nwg-dock
+make get
+make build
+sudo make install
+cd ..
+
+echo "Building nwg-drawer..."
+git clone https://github.com/nwg-piotr/nwg-drawer
+cd nwg-drawer
+make get
+make build
+sudo make install
+cd ..
+
+echo "Building nwg-bar..."
+git clone https://github.com/nwg-piotr/nwg-bar
+cd nwg-bar
+make get
+make build
+sudo make install
+cd ..
